Fix of possible numerical instabilities in 3D to 2D projection

datasets/semantic_kitti.py
@@ -130,7 +130,7 @@ class InstanceCutMix:
                 object = self.bank[id_class][which_one[ii]]
                 object = np.fromfile(object, dtype=np.float32).reshape((-1, 4))
                 # Augment instance
-                label = np.ones((object.shape[0],), dtype=np.int) * id_class
+                label = np.ones((object.shape[0],), dtype=np.int32) * id_class
                 object, label = self.rot(object, label)
                 # Move instance at point p
                 object[:, :3] += p[:3][None]

waffleiron/backbone.py
@@ -34,6 +34,11 @@ class myLayerNorm(nn.LayerNorm):
     def forward(self, x):
         return super().forward(x.transpose(1, -1)).transpose(1, -1)
 
+NORM_OPTIONS = {
+	"batchnorm": nn.BatchNorm1d,
+	"layernorm": myLayerNorm,
+}
+
 
 class DropPath(nn.Module):
     """
@@ -65,14 +70,11 @@ class DropPath(nn.Module):
 
 
 class ChannelMix(nn.Module):
-    def __init__(self, channels, drop_path_prob, layer_norm=False):
+    def __init__(self, channels, drop_path_prob, which_norm="batchnorm"):
         super().__init__()
         self.compressed = False
-        self.layer_norm = layer_norm
-        if layer_norm:
-           self.norm = myLayerNorm(channels)
-        else:
-           self.norm = nn.BatchNorm1d(channels)
+        self.which_norm = which_norm
+        self.norm = NORM_OPTIONS[which_norm](channels)
         self.mlp = nn.Sequential(
             nn.Conv1d(channels, channels, 1),
             nn.ReLU(inplace=True),
@@ -84,8 +86,12 @@ class ChannelMix(nn.Module):
         self.drop_path = DropPath(drop_path_prob)
 
     def compress(self):
-        if self.layer_norm:
-            raise Exception("Compression of ChannelMix layer in WaffleIron has not been implemented with layer norm.")
+        if self.which_norm == "layernorm":
+            warnings.warn(
+                "Compression of ChannelMix layer has not been implemented " +
+                "with layer norm. Skipping compression."
+            )
+            return
         # Join Batch norm and first conv
         norm_weight = self.norm.weight.data / torch.sqrt(
             self.norm.running_var.data + 1e-05
@@ -114,14 +120,11 @@ class ChannelMix(nn.Module):
 
 
 class SpatialMix(nn.Module):
-    def __init__(self, channels, grid_shape, drop_path_prob, layer_norm=False):
+    def __init__(self, channels, grid_shape, drop_path_prob, which_norm="batchnorm"):
         super().__init__()
         self.compressed = False
         self.H, self.W = grid_shape
-        if layer_norm:
-            self.norm = myLayerNorm(channels)
-        else:
-            self.norm = nn.BatchNorm1d(channels)
+        self.norm = NORM_OPTIONS[which_norm](channels)
         self.ffn = nn.Sequential(
             nn.Conv2d(channels, channels, 3, padding=1, groups=channels),
             nn.ReLU(inplace=True),
@@ -185,16 +188,16 @@ class SpatialMix(nn.Module):
 
 
 class WaffleIron(nn.Module):
-    def __init__(self, channels, depth, grids_shape, drop_path_prob, layer_norm=False):
+    def __init__(self, channels, depth, grids_shape, drop_path_prob, which_norm="batchnorm"):
         super().__init__()
         self.depth = depth
         self.grids_shape = grids_shape
         self.channel_mix = nn.ModuleList(
-            [ChannelMix(channels, drop_path_prob, layer_norm) for _ in range(depth)]
+            [ChannelMix(channels, drop_path_prob, which_norm) for _ in range(depth)]
         )
         self.spatial_mix = nn.ModuleList(
             [
-                SpatialMix(channels, grids_shape[d % len(grids_shape)], drop_path_prob, layer_norm)
+                SpatialMix(channels, grids_shape[d % len(grids_shape)], drop_path_prob, which_norm)
                 for d in range(depth)
             ]
         )
@@ -209,7 +212,7 @@ class WaffleIron(nn.Module):
         batch_size, nb_feat, num_points = tokens.shape
         sp_mat = get_all_projections(
             cell_ind, nb_feat, batch_size, num_points, 
-            occupied_cell, tokens.device, self.grids_shape,
+            occupied_cell, tokens.device, self.grids_shape, tokens.dtype,
         )
 
         # Actual backbone

waffleiron/helper_projection.py
@@ -27,8 +27,8 @@ def projection_3d_to_2d_scatter_reduce(feat, sp_mat, B, C, H, W):
     residual.scatter_reduce_(
         2, 
         sp_mat["inflate"], 
-        feat, 
-        "mean", 
+        feat * sp_mat["mask_zero_padding"], 
+        "sum", 
         include_self=False,
     )
     
@@ -46,17 +46,31 @@ def projection_3d_to_2d_sparse_matrix(feat, sp_mat, *args, **kwargs):
 
 
 def get_all_projections_scatter_reduce(
-    cell_ind, nb_feat, *args, **kwargs
+    cell_ind, nb_feat, batch_size, num_points, occupied_cell, device, grids_shape, dtype,
 ):
-    sp_mat = [
-        {"inflate": cell_ind[:, i:i+1].expand(-1, nb_feat, -1)}
-        for i in range(cell_ind.shape[1])
-    ]
+    sp_mat = []
+    B, C = batch_size, nb_feat
+    occupied_cell = occupied_cell.unsqueeze(1).to(dtype)
+    temp_mask = torch.ones_like(occupied_cell)
+    for i in range(cell_ind.shape[1]):
+        # Pre-compute number of points falling in each 2D cells
+        temp_sp_mat = {"inflate": cell_ind[:, i:i+1], "mask_zero_padding": temp_mask}
+        num_points_per_cell = projection_3d_to_2d_scatter_reduce(
+            occupied_cell, temp_sp_mat, B, 1, *grids_shape[i],
+        )
+        num_points_per_cell = torch.gather(
+            num_points_per_cell, 2, temp_sp_mat["inflate"],
+        )
+        # Store projection information
+        sp_mat += [dict()]
+        sp_mat[i]["mask_zero_padding"] = occupied_cell / (num_points_per_cell + 1e-6)
+        sp_mat[i]["inflate"] = cell_ind[:, i:i+1].expand(-1, C, -1)
+    
     return sp_mat
 
 
 def get_all_projections_sparse_matrices(
-    cell_ind, nb_feat, batch_size, num_points, occupied_cell, device, grids_shape
+    cell_ind, nb_feat, batch_size, num_points, occupied_cell, device, grids_shape, *args, **kwargs,
 ):
     point_ind = (
         torch.arange(num_points, device=device)

waffleiron/segmenter.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import warnings
 import torch.nn as nn
 from .backbone import WaffleIron
 from .embedding import Embedding
@@ -27,13 +27,25 @@ class Segmenter(nn.Module):
         depth,
         grid_shape,
         drop_path_prob=0,
-        layer_norm=False,
+        layer_norm=None, # To maintain comptability with previous version
+        which_norm=None, # Preferred new option
     ):
         super().__init__()
         # Embedding layer
         self.embed = Embedding(input_channels, feat_channels)
         # WaffleIron backbone
-        self.waffleiron = WaffleIron(feat_channels, depth, grid_shape, drop_path_prob, layer_norm)
+        if which_norm is None and layer_norm is None:
+            which_norm = "batchnorm"
+        elif which_norm is None and layer_norm is not None:
+            which_norm = "layernorm" if layer_norm else "batchnorm"
+        else:
+            if (which_norm == "layernorm" and not layer_norm) or \
+                (which_norm == "batchnorm" and layer_norm):
+                warnings.warn(
+                    "Arguments which_norm={which_norm} and layer_norm={layer_norm} " +
+                    "are in conflict. Creating the backbone using which_norm={which_norm}."
+                )
+        self.waffleiron = WaffleIron(feat_channels, depth, grid_shape, drop_path_prob, which_norm)
         # Classification layer
         self.classif = nn.Conv1d(feat_channels, nb_class, 1)
 